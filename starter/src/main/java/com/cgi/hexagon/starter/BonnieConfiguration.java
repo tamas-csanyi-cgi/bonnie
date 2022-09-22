0b03d99a07582ce70b15b5eee0b532bf4dada794
mayerij <istvan.mayer@cgi.com>
2022-09-22 15:25:07 +0200

    bonnie-12 Shop - Place an order main merge 1st step

--- starter/src/main/java/com/cgi/hexagon/starter/BonnieConfiguration.java
+++ starter/src/main/java/com/cgi/hexagon/starter/BonnieConfiguration.java
@@ -1,19 +1,20 @@
 package com.cgi.hexagon.starter;
 
-
 import com.cgi.hexagon.bonnierest.BonnieRestConfiguration;
 import com.cgi.hexagon.businessrules.order.OrderStorage;
 import com.cgi.hexagon.businessrules.order.OrderService;
 import com.cgi.hexagon.businessrules.user.UserService;
 import com.cgi.hexagon.businessrules.user.UserStorage;
-import com.cgi.hexagon.kafka.messages.Sender;
+import com.cgi.hexagon.commplugin.kafka.TopicConfig;
+import com.cgi.hexagon.commplugin.kafka.KafkaMessageService;
+import com.cgi.hexagon.commplugin.kafka.OrderConsumer;
 import com.cgi.hexagon.h2storage.H2StorageApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({BonnieRestConfiguration.class, H2StorageApplication.class, Sender.class})
+@Import({BonnieRestConfiguration.class, H2StorageApplication.class, TopicConfig.class, KafkaMessageService.class, OrderConsumer.class})
 public class BonnieConfiguration {
 
     @Bean
@@ -22,5 +23,5 @@ public class BonnieConfiguration {
     }
 
     @Bean
-    UserService userService(com.cgi.hexagon.businessrules.user.UserStorage userStorage) { return new UserService(userStorage); }
+    UserService userService( UserStorage userStorage) { return new UserService(userStorage); }
 }
