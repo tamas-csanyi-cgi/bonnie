d7f25506bf096d91cc470be7f2b74eb6bb519af4
tamas.csanyi@cgi.com <tamas.csanyi@cgi.com>
2022-10-18 12:36:32 +0200

    fix CORS configuration

--- bonnie-rest/src/main/java/com/cgi/bonnie/configuration/BonnieRestConfiguration.java
+++ bonnie-rest/src/main/java/com/cgi/bonnie/configuration/BonnieRestConfiguration.java
@@ -6,14 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.channel.ChannelProcessingFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Arrays;
 
 @Configuration
 @ComponentScan(basePackages = {"com.cgi.bonnie.bonnierest"})
@@ -22,20 +16,18 @@ public class BonnieRestConfiguration implements WebMvcConfigurer {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.antMatcher("/h2").anonymous()
-             .and().addFilterAfter(new CorsFilter(corsConfigurationSource()), ChannelProcessingFilter.class);
+        http.antMatcher("/h2").anonymous();
         return http.build();
     }
 
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
-        configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("authorization", "content-type", "x-auth-token")
+                .exposedHeaders("x-auth-token");
+        WebMvcConfigurer.super.addCorsMappings(registry);
     }
 
 }
