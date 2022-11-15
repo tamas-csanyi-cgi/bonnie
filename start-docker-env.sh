a3e2b8d5d29d97e35f7bf0652933e03b8b814e59
tamas.csanyi@cgi.com <tamas.csanyi@cgi.com>
2022-11-15 14:56:05 +0100

    add container naming, fix starter shell script

--- start-docker-env.sh
+++ start-docker-env.sh
@@ -1,3 +1,6 @@
 #until we're missing docker-compose we can use this file
-docker run -d -p 8081:80 bonnie-frontend
-docker run -d -p 8082:8082 bonnie-backend
+docker stop frontend
+docker stop backend
+docker rm $(docker ps -aq)
+docker run -d -p 8081:80 --name frontend bonnie-frontend
+docker run -d -p 8082:8082 --name backend bonnie-backend
