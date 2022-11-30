106c7390591b369f1e75ffd676863368662f325d
gabor.toth <gabor.toth@cgi.com>
2022-11-30 10:41:42 +0100

    add test data sql script, improved BaseIT class

--- starter/src/test/java/com/cgi/bonnie/BaseIT.java
+++ starter/src/test/java/com/cgi/bonnie/BaseIT.java
@@ -1,23 +1,76 @@
 package com.cgi.bonnie;
 
+import com.cgi.bonnie.authentication.controller.LoginData;
+import com.cgi.bonnie.businessrules.user.User;
+import com.cgi.bonnie.businessrules.user.UserCredentialStorage;
+import com.cgi.bonnie.businessrules.user.UserCredentials;
+import com.cgi.bonnie.businessrules.user.UserStorage;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-@EmbeddedKafka(brokerProperties = { "listeners=PLAINTEXT://localhost:9092", "port=9092" })
 @Transactional
 @AutoConfigureMockMvc
+@TestInstance(PER_CLASS)
+@EmbeddedKafka(brokerProperties = { "listeners=PLAINTEXT://localhost:9092", "port=9092" })
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 class BaseIT {
+
+    static final String TEST_EMAIL = "integration.test@admin.local";
 
     final ObjectMapper objectMapper = new ObjectMapper();
 
     @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    UserStorage userStorage;
+
+    @Autowired
+    UserCredentialStorage userCredentialStorage;
+
+    @Autowired
     MockMvc mockMvc;
+
+    UserCredentials userCredentials;
+
+    User userData;
+
+    @BeforeAll
+    void setup() {
+        userCredentials = userCredentialStorage.findByEmail(TEST_EMAIL);
+        userData = userStorage.findByEmail(TEST_EMAIL);
+    }
+
+    LoginData getLoginData() {
+        final LoginData loginData = new LoginData();
+        loginData.setUsername(userCredentials.getEmail());
+        loginData.setPassword(userCredentials.getPassword());
+        return loginData;
+    }
+
+    SecurityContext getSecurityContext() {
+        final LoginData loginData = getLoginData();
+        final SecurityContext securityContext = SecurityContextHolder.getContext();
+
+        final UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginData.getUsername(), loginData.getPassword());
+        final Authentication authentication = authenticationManager.authenticate(authenticationToken);
+
+        securityContext.setAuthentication(authentication);
+        return securityContext;
+    }
 }
---
+++ starter/src/test/resources/data.sql
@@ -0,0 +1,1 @@
+insert into assembly_user (role, email, name ,password) values('ADMIN', 'integration.test@admin.local', 'IT ADMIN', 'password');
